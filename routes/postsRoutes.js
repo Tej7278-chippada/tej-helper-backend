668536de3a9798055ce0d53f29b4449659515b7b
Tej7278-chippada <tejachippada45@gmail.com>
2025-06-12 16:23:10 +0530

    # postsRoutes api.js changes
    ✍️ for fetching only the post's entire media while editing post's data on PostService.js new API "fetchPostMediaById" added
    
    post media returns well but cant taking on PostService existing media ✍️

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -268,6 +268,27 @@ router.get('/my-posts', authMiddleware, async (req, res) => {
   }
 });
 
+// Get posts media only for updating post by authenticated user
+// router.get('/postMedia/:postId', authMiddleware, async (req, res) => {
+//   try {
+//     const { postId } = req.params;
+//     const post = await postsModel.findById(postId);
+    
+//     if (!post) {
+//       return res.status(404).json({ message: 'Post not found' });
+//     }
+
+//     const postWithBase64Media = {
+//       media: post.media.map((buffer) => buffer.toString('base64')),
+//     };
+    
+//     res.status(200).json( postWithBase64Media );
+//   } catch (err) {
+//     console.error('Error fetching user posts:', err);
+//     res.status(500).json({ message: 'Failed to fetch user posts' });
+//   }
+// });
+
 // Get all products (public)
 // router.get('/all', async (req, res) => {
 //   try {
--- server.js
+++ server.js
@@ -256,4 +256,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5012;
-server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port http://192.168.0.106:${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port http://192.168.0.103:${PORT}`));
